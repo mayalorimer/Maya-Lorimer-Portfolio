f0d42ad5dc9c7002a7e76a54ed875706d2d2e5cd
Maya Lorimer <maya.lorimer@yahoo.com>
2022-07-28 18:15:29 -0700

    additional pages added

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
 import Home from './pages/Home';
-import About from './pages/About';
-import Blog from './pages/Blog';
+import About from './pages/AboutMe';
+import Resume from './pages/Resume';
 import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
@@ -17,9 +18,12 @@ export default function PortfolioContainer() {
     if (currentPage === 'About') {
       return <About />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === 'Resume') {
+      return <Resume />;
     }
+    if (currentPage === 'Portfolio') {
+        return <Portfolio />;
+      }
     return <Contact />;
   };
 
---
+++ src/components/navTabs.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+// TODO: Add a comment explaining how we are able to extract the key value pairs from props
+// using the destructuring method, use this instead of props so you dont have to do props.pageChange, etc. 
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#home"
+          onClick={() => handlePageChange('Home')}
+          //*  TODO: BONUS: Add a comment explaining what kind of operator this is and what it is checking for
+          // conditional operator, it is checking if the first statement is true then it changes to 'nav-link active'
+          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          //  TODO: Add a comment explaining what this logic is doing
+
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About Me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
+          //  TODO: Add a comment explaining what this logic is doing
+          // if the current page variable is equal to blog it is changing the nav-link class to active for the blog, otherwise it is removing the active class
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          //  TODO: Add a comment explaining what this logic is doing
+
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact Me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          //  TODO: Add a comment explaining what this logic is doing
+
+          onClick={() => handlePageChange('Resume')}
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
