84533c6667a6b199a2f82205439512ddd99dbe70
Maya Lorimer <maya.lorimer@yahoo.com>
2022-07-29 22:09:22 -0700

    links added

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import portfolio from '../../portfolio.json'
 import ProjectCard from './projectCard';
@@ -13,19 +11,6 @@ export default function Portfolio() {
       <Row xs={1} md={2} className="g-4">
       {portfolio.map((project, index) => (
         <ProjectCard key={project.id} image={project.image} name={project.name} github={project.github} deploy={project.deploy}/>
-/*         <Col key={index}>
-          <Card>
-            <Card.Img variant="top" src={project.image}/>
-            <Card.Body>
-              <Card.Title>{project.name}</Card.Title>
-              {project.image}
-              <Card.Text>
-                <a href={portfolio.github}>Github </a>
-                <a href={portfolio.deployed}>Deployed app</a>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col> */
       ))}
     </Row>
   
