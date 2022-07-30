5783238bef89c2a4a952b92f4478029e80c168c2
Maya Lorimer <maya.lorimer@yahoo.com>
2022-07-29 18:30:13 -0700

    Resume page updated

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,18 +5,28 @@ export default function Resume() {
     <div>
       <h1>Resume</h1>
       <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+        Download my <a href="https://drive.google.com/file/d/17N1_kg_InoAd2l3_A-Ev4u9fDFwI4Bq4/view?usp=sharing" >Resume</a>
       </p>
+      <h3>Front-End Proficiencies</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>Responsive design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
+      <h3>Back-End Proficiencies</h3>
+      <ul>
+      <li>APIs</li>
+      <li>Node</li>
+      <li>Express</li>
+      <li>REST</li>
+      <li>MySQL, Sequelize</li>
+      <li>NoSQL</li>
+      <li>MongoDB, Mongoose</li>
+      </ul>
     </div>
   );
 }
